952366152fa722fb40e2868e9475c0c857acaaa7
Lait96 <79802473+Lait96@users.noreply.github.com>
2021-03-06 21:35:46 +0300

    Add files via upload

---
+++ Is-ascending.py
@@ -0,0 +1,19 @@
+# -*- coding: utf_8 -*-
+
+# Задача:
+# Проверить является ли список монотонно возрастающим
+# Доп. условия:
+# Решение в виде функции с одним циклом while без вложенных условий и циклов
+
+List_a = list(map(int, input().split()))
+
+
+def IsAscending(A):
+    i, size = 1, len(A)
+    while i < size and A[i - 1] < A[i]:
+        i += 1
+    return "YES" if i == size else "NO"
+
+
+result = IsAscending(List_a)
+print(result)
